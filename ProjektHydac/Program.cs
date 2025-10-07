784b9ad1301c3fd5a9efa7de7b67ce7e06e742eb
Robert Marker Andersen <robertmarkera@gmail.com>
2025-10-07 12:05:06 +0200

    Måske færdig kode

--- ProjektHydac/Program.cs
+++ ProjektHydac/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Hydac123
 {
@@ -9,50 +10,64 @@ namespace Hydac123
             GuestLog guestlog = new GuestLog();
             bool kør = true;
 
-
-
-
-
-
-
-
             while (kør)
             {
-                Console.WriteLine("╔════════════════════════════════╗");
-                Console.WriteLine("║        HYDAC GÆSTELOG          ║");
-                Console.WriteLine("╠════════════════════════════════╣");
-                Console.WriteLine("║ 1. Tilføj ny gæst              ║");
-                Console.WriteLine("║ 2. Vis alle gæster             ║");
-                Console.WriteLine("║ 3. Rediger afgang              ║");
-                Console.WriteLine("╚════════════════════════════════╝");
-                Console.Write("Vælg en mulighed (1-3): ");
-                string valg = Console.ReadLine();
+                Console.WriteLine("+----------------------------------+");
+                Console.WriteLine("|           HYDAC LOGIN            |");
+                Console.WriteLine("|----------------------------------|");
+                Console.WriteLine("|  Indtast venligst kodeordet      |");
+                Console.WriteLine("|  for at få adgang til systemet.  |");
+                Console.WriteLine("+----------------------------------+");
+                Console.Write("Kodeord: ");
+                string kodeord = Console.ReadLine();
                 Console.Clear();
 
-
-                switch (valg)
+                if (kodeord == "123")
                 {
-                    case "1":
-                        guestlog.AddGuest();
+
+                    while (kør)
+                    {
+                        Console.WriteLine("+--------------------------+");
+                        Console.WriteLine("|      HYDAC GÆSTELOG      |");
+                        Console.WriteLine("|--------------------------|");
+                        Console.WriteLine("|  1. Tilføj ny gæst       |");
+                        Console.WriteLine("|  2. Vis alle gæster      |");
+                        Console.WriteLine("|  3. Tjek gæst ud         |");
+                        Console.WriteLine("+--------------------------+");
+                        Console.Write("Vælg en mulighed (1-3): ");
+                        string valg = Console.ReadLine();
                         Console.Clear();
-                        break;
 
 
-                    case "2":
-                        guestlog.ShowGuestlog();
-                        Console.WriteLine("\n" + "Tryk på en tilfældig knap for at komme tilbage til menuen");
-                        Console.ReadLine();
-                        Console.Clear();
-                        break;
+                        switch (valg)
+                        {
+                            case "1":
+                                guestlog.AddGuest();
+                                Console.Clear();
+                                break;
 
-                    case "3":
-                        Console.Write("Indtast navn på gæst der skal redigeres: ");
-                        string name = Console.ReadLine();
-                        guestlog.EditGuest(name);
-                        Console.ReadLine();
-                        Console.Clear();
-                        break;
+                            case "2":
+                                guestlog.ShowGuestlog();
+                                Console.WriteLine("\n" + "Tryk på en tilfældig knap for at komme tilbage til menuen");
+                                Console.ReadLine();
+                                Console.Clear();
+                                break;
 
+                            case "3":
+                                Console.Write("Indtast navn på gæst der skal redigeres: ");
+                                string name = Console.ReadLine();
+                                guestlog.EditGuest(name);
+                                Console.ReadLine();
+                                Console.Clear();
+                                break;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Forkert kodeord :/");
+                    Console.ReadLine();
+                    Console.Clear();
                 }
             }
         }
